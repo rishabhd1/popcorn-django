9058c8de05561b5ae0029e8dca2db685a36ae012
Rishabh Dubey <rishabhdubey020@gmail.com>
2019-03-27 23:33:33 +0530

    added blank and null as false in name of actor and producer model

--- actor/models.py
+++ actor/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Actor (models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, blank=False, null=False)
     sex = models.CharField(max_length=20)
     dateOfBirth = models.DateField()
     bio = models.TextField()
