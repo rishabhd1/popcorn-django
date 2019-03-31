fbe6a786a717a364ff3dd3c55e36c90cea7facc2
Rishabh Dubey <rishabhdubey020@gmail.com>
2019-03-31 18:17:34 +0530

    added urls in actor formatted some files and incomplete actor html

--- actor/views.py
+++ actor/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+class ActorView(TemplateView):
+    template_name = "actors/actors.html"
