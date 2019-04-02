35e838797dfffae8dac8db28f23e0ba466d532e6
Rishabh Dubey <rishabhdubey020@gmail.com>
2019-04-03 00:26:29 +0530

    added actor detail view

--- actor/urls.py
+++ actor/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from actor.views import ActorList
+from actor.views import ActorList, ActorDetail
 
 urlpatterns = [
     path('', ActorList.as_view(), name='actors'),
+    path('<int:id>/', ActorDetail.as_view(), name='actor_detail')
 ]
